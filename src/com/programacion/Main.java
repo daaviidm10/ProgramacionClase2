5c640696905949c97a409c3dee7e978299e60b0a
dam1b <dmarinoalonso@danielcastelao.org>
2020-10-27 11:32:55 +0100

    Modificacion 2

--- src/com/programacion/Main.java
+++ src/com/programacion/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
     System.out.println("Principal");
-    System.out.println("Paquete principalmente");
+    System.out.println("Paquete principalmentr");
     }
 }
