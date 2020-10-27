ed8215c93fba37fe63263c591c4b12416ea1efd9
dam1b <dmarinoalonso@danielcastelao.org>
2020-10-27 11:27:30 +0100

    Modificacion

---
+++ src/com/programacion/Main.java
@@ -0,0 +1,9 @@
+package com.programacion;
+
+public class Main {
+
+    public static void main(String[] args) {
+    System.out.println("Principal");
+    System.out.println("Paquete principal");
+    }
+}
