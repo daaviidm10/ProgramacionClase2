3fccb3173bd1152117d652186056623352f580ad
David Mariño Alonso <dmarinoalonso@danielcastelao.org>
2020-10-30 08:51:59 +0100

    Ejercicio resuelto modified

--- src/com/programacion/boletin2/ejercicio2.java
+++ src/com/programacion/boletin2/ejercicio2.java
@@ -2,6 +2,6 @@ package com.programacion.boletin2;
 
 public class ejercicio2 {
     public static void main(String[] args) {
-        System.out.println("Ejercicio1 Boletin 1");
+        System.out.println("Ejercicio2 Boletin 1");
     }
 }
