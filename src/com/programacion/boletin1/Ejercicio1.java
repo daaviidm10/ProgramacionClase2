30fcd6ac4f79ad4b59e3bc45e1758fe3bfd9623c
dam1b <dmarinoalonso@danielcastelao.org>
2020-11-12 12:32:30 +0100

    Bol6

--- src/com/programacion/boletin1/Ejercicio1.java
+++ src/com/programacion/boletin1/Ejercicio1.java
@@ -1,7 +1,25 @@
 package com.programacion.boletin1;
 
+/**
+ * Datos
+ * @author David Mariño Alonso
+ * @version 0.0.1
+ *
+ */
 public class Ejercicio1 {
+    /**
+     * Detalles del método
+     *
+     */
     public static void main(String[] args) {
-        System.out.println("Ejercicio1 Boletin 1");
     }
+    /**
+     *
+     * @param radio corresponde ao valor do radio.
+     * @return corresponde á resolución da operación.
+     */
+    public float calcular(double radio){
+        return (float) (radio*2*3.14);
+    }
+
 }
