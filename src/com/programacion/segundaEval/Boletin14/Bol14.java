b0644e825da40944fc84b1d45fe932d7691bd1e6
David Mariño Alonso <dmarinoalonso@danielcastelao.org>
2021-02-03 20:15:26 +0100

    Boletin 14

--- src/com/programacion/segundaEval/Boletin14/Bol14.java
+++ src/com/programacion/segundaEval/Boletin14/Bol14.java
@@ -2,14 +2,7 @@ package com.programacion.segundaEval.Boletin14;
 
 public class Bol14 {
     public static void main(String[] args) {
-        ConvetirTemp temperatura1 = new ConvetirTemp();
-        ConvetirTemp temperatura2 = new ConvetirTemp();
-        try {
-            System.out.println("Grados Farenheit "+temperatura1.centigradosAFarenheit(78));
-        }catch (TemperaturaErradaException error){
-            System.out.println(error.getMessage());
-        }
-        temperatura2.centigradosAReamur(46F);
+
     }
 }
-}
+
--- src/com/programacion/segundaEval/Boletin14/Temperatura.java
+++ src/com/programacion/segundaEval/Boletin14/Temperatura.java
@@ -1,27 +1,4 @@
 package com.programacion.segundaEval.Boletin14;
 
 public class Temperatura {
-    public static final float TEMPERATURA_MINIMA =80F; // creo una constante estática de clase con el valor mínimo
-
-    public ConversorTemperaturas() {
-    }
-    public float centigradosAFarenheit(float temperaturaCentigrados) throws TemperaturaErradaException{ // utilizo el throws para decir que el método no va a capturar la excepció (try)
-        if (temperaturaCentigrados<TEMPERATURA_MINIMA)
-            throw new TemperaturaErradaException("Introduzca una temperatura superior a 80º"); // creo creo una nueva excepción si la temperatura es inferior a 80º y le paso un mensaje personal porque lo tengo en así en el constructor de la clase que trata excepciones.
-        else {
-            return (temperaturaCentigrados * 1.8F) + 32.4F;
-        }
-    }
-    public void centigradosAReamur (float celsius){
-        try {  // Este método captura la excepción para tratarla
-            float temperaturaReamur=0.8F*celsius;
-            if (celsius<TEMPERATURA_MINIMA) {
-                throw new TemperaturaErradaException(); // Creo un objeto en la clase por si salta la excepción poder ser tratada.
-            }
-            System.out.println("temperatura en celsius= " + temperaturaReamur); //Si no salta excepción ejecuta esta linea sin problema
-        }catch (TemperaturaErradaException es){ // si salta excepción creo el objeto es en nuestra clase
-            System.out.println(es.getMessage()); // Imprimo el mensaje que tengo en la clase creado.
-        }
-    }
-
 }
