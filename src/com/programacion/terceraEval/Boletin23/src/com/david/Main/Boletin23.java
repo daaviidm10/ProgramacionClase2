3ef11e6ebda9c57d3f838ff91f9234a65756ed2b
daaviidm10 <72596994+daaviidm10@users.noreply.github.com>
2021-05-03 17:32:55 +0200

    Add files via upload

---
+++ src/com/programacion/terceraEval/Boletin23/src/com/david/Main/Boletin23.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.david.Main;
+
+import Calculadora.Calculadora;
+
+/**
+ *
+ * @author dmama
+ */
+public class Boletin23 {
+     public static void main (String[] args){
+       Calculadora obx =new Calculadora();
+       obx.setVisible(true);
+     }
+}
