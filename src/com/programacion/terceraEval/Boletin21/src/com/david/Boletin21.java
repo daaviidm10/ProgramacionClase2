bc7ae407170398e21de2188df52caaa8a1842851
daaviidm10 <72596994+daaviidm10@users.noreply.github.com>
2021-05-03 18:28:27 +0200

    Add files via upload

---
+++ src/com/programacion/terceraEval/Boletin21/src/com/david/Boletin21.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.david;
+
+import Ventana1.Usuario;
+import Ventana2.Devoluciones;
+
+/**
+ *
+ * @author dmama
+ */
+public class Boletin21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Usuario ventana1 = new Usuario();
+        ventana1.setVisible(true);
+        
+        Devoluciones ventana2 = new Devoluciones();
+        ventana2.setVisible(true);
+    }
+    
+}
