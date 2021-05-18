f1228f1bea8d2f423cd63197488ae86be5efdbc8
daaviidm10 <72596994+daaviidm10@users.noreply.github.com>
2021-05-18 22:17:33 +0200

    Proxecto

---
+++ src/com/programacion/terceraEval/ProyectoBD/src/proyectobd/ProyectoBD.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectobd;
+
+import Ventana.Xogadoras;
+
+/**
+ *
+ * @author dmama
+ */
+public class ProyectoBD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Xogadoras xoga = new Xogadoras();
+        xoga.setResizable(false);
+        xoga.setVisible(true);
+    }
+    
+}
